a662f5e5f96bb5e42c297c1cf82bae733747246e
אביב לזר <avrv10@h-MacBook-Pro-sl-byb.local>
2018-10-29 17:43:13 +0200

    Aviv Test

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,6 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.err.println("Liran nachman");
+		System.err.println("AVIV LAZAR SHOLET");
 	}
 }
