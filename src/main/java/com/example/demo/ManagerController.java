aeb13329fe1f244d59d4c434c7ab4346a87d0e19
unknown <talisraeli.t@gmail.com>
2018-10-31 11:12:39 +0200

    new class of manager

---
+++ src/main/java/com/example/demo/ManagerController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/manager")
+public class ManagerController {
+
+// Tal: POST was still not learned, will do it when he teaches POST
+	
+}
