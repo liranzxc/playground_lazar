0576d81bb82e7a16364c1337a6368d2a8a89a47f
Liran Nachman <lirannh@gmail.com>
2018-11-23 18:53:46 +0200

    test element liran

--- src/test/java/com/example/demo/ElementTests/ElementTestShay.java
+++ src/test/java/com/example/demo/ElementTests/ElementTestShay.java
@@ -16,8 +16,6 @@ import org.junit.Test;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,44 +37,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ElementTestShay {
 
 	private ElementEntity demo_entity;
-	
+
 	/*
-	 * 	====================================== READ ME =====================================================
-	 * 	In this test class i do:
-	 *  - POST /playground/elements/{userPlayground }/{email}    				I/O:  ElementTO | ElementTO
-	 *  - PUT  /playground/elements/{userPlayground}/{email}/{playground}/{id} 	I/O:  ElementTO | ---------
-	 *  - GET /playground/elements/{userPlayground}/{email}/{playground}/{id}   I/O:  --------- | ElementTO
-	 *  ====================================== READ ME =====================================================
+	 * ====================================== READ ME
+	 * ===================================================== In this test class i
+	 * do: - POST /playground/elements/{userPlayground }/{email} I/O: ElementTO |
+	 * ElementTO - PUT
+	 * /playground/elements/{userPlayground}/{email}/{playground}/{id} I/O:
+	 * ElementTO | --------- - GET
+	 * /playground/elements/{userPlayground}/{email}/{playground}/{id} I/O:
+	 * --------- | ElementTO ====================================== READ ME
+	 * =====================================================
 	 */
-	
+
 	@LocalServerPort
 	private int port;
-	
+
 	private String url;
-	
+
 	private RestTemplate restTemplate;
 	private ObjectMapper jsonMapper;
-	
+
 	@Autowired
 	private IElementService elementService;
-	
 
 	@PostConstruct
 	public void init() {
 		this.restTemplate = new RestTemplate();
 		this.jsonMapper = new ObjectMapper();
 		this.url = "http://localhost:" + port + "/playground/elements";
-		
+
 		System.err.println(this.url);
-		
+
 	}
-		
+
 	@Before
 	public void setup() {
 
-		this.demo_entity = new ElementEntity("lazar", "1", new Location(),
-											"demo", new Date(), null, "demo type", 
-											 null, "Shay", "ggwp@123.com");
+		this.demo_entity = new ElementEntity("lazar", "1", new Location(), "demo", new Date(), null, "demo type", null,
+				"Shay", "ggwp@123.com");
 	}
 
 	@After
@@ -89,67 +88,55 @@ public class ElementTestShay {
 		String usrPlayground = "lazar_playground";
 		String email = "demo@gmail.com";
 		ElementTO eto = new ElementTO(this.demo_entity);
-		
+
 		boolean success = false;
-		
-		//when
+
+		// when
 		try {
-			this.restTemplate.postForObject(this.url +  "/{userPlayground}/{email}", eto, ElementTO.class, usrPlayground, email);
+			this.restTemplate.postForObject(this.url + "/{userPlayground}/{email}", eto, ElementTO.class, usrPlayground,
+					email);
 			success = true;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			// do nothing
 		}
-		
-		//that
+
+		// that
 		assertTrue(success);
 	}
-	
-	@Test(expected=elementAlreadyExistException.class)
+
+	@Test(expected = elementAlreadyExistException.class)
 	public void createElementWhenElementAlreadyExist() throws elementAlreadyExistException {
-		//given 
+		// given
 		String userPlayground = "lazar_playground";
 		String email = "demo@gmail.com";
 		ElementTO eto = new ElementTO(demo_entity);
-		
-//		boolean dataEnterd = false;
-//		
-//		try {
-//			this.elementService.addNewElement(eto.ToEntity());
-//			dataEnterd = true;
-//		}
-//		catch(elementAlreadyExistException e) {
-//			//do nothing
-//		}
-//		
-//		if(dataEnterd) {
-//			this.restTemplate.postForObject(this.url +  "/{userPlayground}/{email}", eto, ElementTO.class, userPlayground, email);
-//		}
+
 //		
 		boolean dataEnterd = false;
-		
+
 		try {
 			this.elementService.addNewElement(eto.ToEntity());
 			dataEnterd = true;
+		} catch (elementAlreadyExistException e) {
+			// do nothing
+			// its good
 		}
-		catch(elementAlreadyExistException e) {
-			//do nothing
-		}
-		
-		if(dataEnterd) {
+
+		if (dataEnterd) {
+
 			this.elementService.addNewElement(eto.ToEntity());
+
 		}
 	}
-	
+
 	@Test
 	public void updateElement() {
-		
+
 	}
-	
+
 	@Test
 	public void getSpecificElement() {
-		
+
 	}
-	
-	
+
 }
